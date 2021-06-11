8245a4f36e8c515f562d93d10cff225cbd53506c
Hedilton <hediltoncolana@gmail.com>
2021-06-11 15:52:45 -0500

    Agregar archivos de proyecto.

---
+++ EjercicioWinform2Hilos/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace EjercicioWinform2Hilos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picture
+            // 
+            this.picture.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.picture.Location = new System.Drawing.Point(63, 145);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(148, 84);
+            this.picture.TabIndex = 1;
+            this.picture.TabStop = false;
+            // 
+            // picture2
+            // 
+            this.picture2.Location = new System.Drawing.Point(358, 145);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(138, 84);
+            this.picture2.TabIndex = 2;
+            this.picture2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 320);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.PictureBox picture2;
+    }
+}
+
---
+++ EjercicioWinform2Hilos/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace EjercicioWinform2Hilos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread x = new Thread(metodo1);
+            Thread y = new Thread(metodo2);
+            x.Start();y.Start();
+
+        }
+        private void metodo1()
+        {
+            for(int i=1;i<=10;i++)
+            {
+                picture.BackColor = Color.Red;
+                Thread.Sleep(500);
+                picture.BackColor = Color.Green;
+                Thread.Sleep(200);
+                picture.BackColor = Color.Blue;
+                Thread.Sleep(300);
+                picture.BackColor = Color.Black;
+                Thread.Sleep(100);
+
+            }
+        }
+        private void metodo2()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                picture2.BackColor = Color.Yellow;
+                Thread.Sleep(300);
+                picture2.BackColor = Color.BlueViolet;
+                Thread.Sleep(100);
+                picture2.BackColor = Color.DeepSkyBlue;
+                Thread.Sleep(200);
+                picture2.BackColor = Color.LightSalmon;
+                Thread.Sleep(150);
+
+            }
+        }
+    }
+}
